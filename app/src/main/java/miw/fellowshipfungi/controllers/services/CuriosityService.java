6066f20697db7dc5f24aa6343ef66173f8671fdd
Ivan Pulido <ivan.pulido.diaz@alumnos.upm.es>
2024-04-27 18:16:09 +0200

    Curiosity reset streak if it was not entered yesterday

--- app/src/main/java/miw/fellowshipfungi/controllers/services/CuriosityService.java
+++ app/src/main/java/miw/fellowshipfungi/controllers/services/CuriosityService.java
@@ -66,7 +66,7 @@ public class CuriosityService {
                 Date streakLastDate = document.getDate("streakLastDate");
                 int currentStreakInt = 1;
 
-                if (currentStreak == null) {
+                if (currentStreak == null || !this.isYesterday(streakLastDate)) {
                     streakLastDate = new Date();
                 } else if (isSameDay(streakLastDate, new Date())) {
                     return;
@@ -89,7 +89,7 @@ public class CuriosityService {
 
     }
 
-    private boolean isSameDay(Date date1, Date date2) {
+    private boolean isSameDay(Date date1,Date date2) {
         Calendar cal1 = Calendar.getInstance();
         cal1.setTime(date1);
         Calendar cal2 = Calendar.getInstance();
@@ -97,6 +97,13 @@ public class CuriosityService {
         return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
                 cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH) &&
                 cal1.get(Calendar.DAY_OF_MONTH) == cal2.get(Calendar.DAY_OF_MONTH);
+    }
+    private boolean isYesterday(Date date1) {
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+        cal1.add(Calendar.DAY_OF_MONTH, 1);
+
+       return this.isSameDay(cal1.getTime(),new Date());
     }
 
     public interface CuriosityServiceCallback {
