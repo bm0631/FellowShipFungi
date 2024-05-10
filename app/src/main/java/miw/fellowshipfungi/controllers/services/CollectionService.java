a848747dfac429e1a250a551e2f59242cf8625b0
Ivan Pulido <ivan.pulido.diaz@alumnos.upm.es>
2024-05-10 17:32:38 +0200

    Delete enconter and img enconter

--- app/src/main/java/miw/fellowshipfungi/controllers/services/CollectionService.java
+++ app/src/main/java/miw/fellowshipfungi/controllers/services/CollectionService.java
@@ -12,7 +12,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import miw.fellowshipfungi.models.ask.recognitionmodels.RecognitionEntity;
 import miw.fellowshipfungi.models.profile.EnconterCollectionEntity;
-
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 public class CollectionService extends BaseService {
 
     private static CollectionService instance;
@@ -74,15 +75,41 @@ public class CollectionService extends BaseService {
                 .document(this.getUserId())
                 .collection(COLLECTION_ENCONTERS)
                 .document(enconterId)
-                .delete()
-                .addOnSuccessListener(aVoid -> {
-                    Log.d("CollectionService", "Enconter successfully deleted!");
-                    listener.onEnconterDeleted();
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        Map<String, Object> enconterData = documentSnapshot.getData();
+                        String nameImg = (String) enconterData.get("nameImg");
+                        deleteImage(nameImg);
+                    }
+                    db.collection(COLLECTION_PROFILE)
+                            .document(this.getUserId())
+                            .collection(COLLECTION_ENCONTERS)
+                            .document(enconterId)
+                            .delete()
+                            .addOnSuccessListener(aVoid -> {
+                                Log.d("CollectionService", "Enconter successfully deleted!");
+                                listener.onEnconterDeleted();
+                            })
+                            .addOnFailureListener(e -> {
+                                Log.e("CollectionService", "Error deleting enconter", e);
+                                listener.onFailure("Error deleting enconter");
+                            });
                 })
                 .addOnFailureListener(e -> {
-                    Log.e("CollectionService", "Error deleting enconter", e);
-                    listener.onFailure("Error deleting enconter");
+                    Log.e("CollectionService", "Error retrieving enconter data", e);
+                    listener.onFailure("Error retrieving enconter data");
                 });
+    }
+
+
+    private void deleteImage(String nameImg) {
+        if (nameImg != null && !nameImg.isEmpty()) {
+            StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("EncontersImg/" + nameImg);
+            storageRef.delete()
+                    .addOnSuccessListener(aVoid -> Log.d("CollectionService", "Image successfully deleted: " + nameImg))
+                    .addOnFailureListener(e -> Log.e("CollectionService", "Error deleting image: " + nameImg, e));
+        }
     }
 
     public interface OnCollectionListener {
